6e0c5b855b6e9a8642bab86b997a5518f348fa39
Felipe <felipe.campos@al.resilia.com.br>
2020-06-22 15:27:12 -0300

    Commit inicial

---
+++ public/scripts/intro.js
@@ -0,0 +1,53 @@
+// const pessoa = {
+//     altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//      document.write("Run Forrest!")
+//     return " Run Forrest!"
+//   } 
+  
+// }
+
+// document.write(pessoa.correr())
+
+
+// Array - Vetores
+
+
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+//Funções - Funcionalidades - Atalhos - Reuso de código
+
+
+// //regristrou a Função
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// //executando a função
+// sayMyName("Felipe")
+// sayMyName("Valeska")
+// sayMyName("kyam")
+// sayMyName("Patrícia")
+
+//Condicionais
+
+// const notaFinal = 7
+
+// if(notaFinal < 5){
+//    document.write("reprovado")
+//    } else{ 
+//      document.write("Aprovado")
+//      }
+
+//Loop - Repetições
+
+for (i=0; i<10; i++){
+  
+    document.write(`<p> ${i} </p>` )
+  }
+  
